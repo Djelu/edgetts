529b8dfb9b45c4d1e972f6fb11e43846e40dc13a
izhukov <izhukov@btlab.ru>
2023-03-05 15:04:32 +0300

    split by spaces added

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ import io
 import itertools
 import re
 import time
-
+from faker import Faker
 import edge_tts
 
 
 async def foo(buffer_size):
     text = get_text()
-    all_sentences = re.split('[.!?]', text)
-    array_of_sentences = get_splited_sentences(all_sentences, buffer_size)
+    # text = get_fake_text(111111)
+    # start_time = time.time()
+    splited = prepare_splited_text(text, buffer_size, 1000, 2000)
+    # res = list(map(lambda str: {'len': len(str), 'text': str}, splited))
+    # mp3 = await tts_one(0, text)
+
+    # end_time = time.time()
+    # execution_time = end_time - start_time
+    # print(f"time = {execution_time}")
+
+    # mp3 = mp3[0]
+    # with open("testText.mp3", "wb") as f:
+    #     f.write(mp3)
+    # all_sentences = re.split('[.!?]', text)
+    array_of_sentences = get_splited_sentences(splited, buffer_size)
     all_mp3_parts = await run_it_with_buffer(array_of_sentences, buffer_size)
     write_to_file(all_mp3_parts)
     i = 0
@@ -26,6 +39,10 @@ async def run_it_with_buffer(sentences, buffer_size):
         mp3_parts = await tts_all(buffered_sentences, ext_num)
         result.append(mp3_parts)
 
+        # for mp3_part in mp3_parts:
+        #     with open(f"test/test_{buffer_index+1}.mp3", "wb") as f:
+        #         f.write(mp3_part[0])
+
         end_time = time.time()
         execution_time = end_time - start_time
         sum_time = sum_time + execution_time
@@ -38,6 +55,29 @@ def get_splited_sentences(sentences, count):
     return [sentences[i:i + count] for i in range(0, len(sentences), count)]
 
 
+def prepare_splited_text(text, buffer_size, first_strings_len, last_strings_len):
+    words = text.split(" ")
+    first_part = strip_strings(words, first_strings_len, buffer_size)
+    last_part = strip_strings(words[first_part['last_word_index']+1:], last_strings_len)
+    return [*first_part['result'], *last_part['result']]
+
+
+def strip_strings(words, strings_len, buffer_size=None):
+    result = []
+    s = ""
+    last_word_index = 0
+    for index, word in enumerate(words):
+        s += word + " "
+        if len(s) > strings_len:
+            result.append(s.strip())
+            if buffer_size is None or len(result) < buffer_size:
+                s = ""
+            else:
+                last_word_index = index
+                break
+    return {'result': result, 'last_word_index': last_word_index}
+
+
 async def tts_all(sentences, ext_num):
     tasks = [tts_one(index + ext_num, sentence) for index, sentence in enumerate(sentences)]
     return await asyncio.gather(*tasks)
@@ -45,7 +85,7 @@ async def tts_all(sentences, ext_num):
 
 async def tts_one(index, text):
     print(f"th{index + 1} started")
-    communicate = edge_tts.Communicate(text, 'ru-RU-SvetlanaNeural')
+    communicate = edge_tts.Communicate(text, 'ru-RU-SvetlanaNeural', rate = "+100%")
     bytes_io = io.BytesIO()
     async for chunk in communicate.stream():
         if chunk["type"] == "audio":
@@ -68,12 +108,16 @@ def write_to_file(mp3_parts):
 
 
 def get_text():
-    with open('text.txt', 'r', encoding='utf-8') as file:
+    with open('textFull.txt', 'r', encoding='utf-8') as file:
         text = file.read()
     return text
 
 
+def get_fake_text(length):
+    faker = Faker('ru_RU')
+    return faker.text(max_nb_chars=length)
+
+
 if __name__ == '__main__':
     asyncio.run(foo(20))
-    # proxies = proxy_service.get_proxy_info(100)
     i = 0
