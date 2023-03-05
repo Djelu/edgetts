3fcf0dd0a34277cb47928ad82b5b9554a8fca65b
izhukov <izhukov@btlab.ru>
2023-03-05 13:06:19 +0300

    fix

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import asyncio
 import io
-import re
-import numpy as np
-import math
-import time
-import edge_tts
 import itertools
-import proxy_service
+import re
+import time
+
+import edge_tts
 
 
 async def foo(buffer_size):
@@ -31,34 +29,32 @@ async def run_it_with_buffer(sentences, buffer_size):
         end_time = time.time()
         execution_time = end_time - start_time
         sum_time = sum_time + execution_time
-        print(f"{buffer_index+1}/{len(sentences)} completed by {execution_time}")
+        print(f"{buffer_index + 1}/{len(sentences)} completed by {execution_time}")
     print(f"all converted by {sum_time}")
     return result
 
 
 def get_splited_sentences(sentences, count):
-    # counts_in_one_part = math.ceil(len(sentences) / count)
     return [sentences[i:i + count] for i in range(0, len(sentences), count)]
 
 
 async def tts_all(sentences, ext_num):
-    tasks = [tts_one(index+ext_num, sentence) for index, sentence in enumerate(sentences)]
+    tasks = [tts_one(index + ext_num, sentence) for index, sentence in enumerate(sentences)]
     return await asyncio.gather(*tasks)
 
 
-async def tts_one(index, sentences):
-    print(f"th{index+1} started")
-    text = " ".join(sentences)
+async def tts_one(index, text):
+    print(f"th{index + 1} started")
     communicate = edge_tts.Communicate(text, 'ru-RU-SvetlanaNeural')
     bytes_io = io.BytesIO()
     async for chunk in communicate.stream():
         if chunk["type"] == "audio":
             bytes_io.write(chunk["data"])
     audio_bytes = bytes_io.getvalue()
-    with open(f"test/test_{index+1}.mp3", "wb") as f:
-        f.write(audio_bytes)
-    with open(f"test/test_{index + 1}.txt", "wb") as f:
-        f.write(text.encode("utf-8"))
+    # with open(f"test/test_{index+1}.mp3", "wb") as f:
+    #     f.write(audio_bytes)
+    # with open(f"test/test_{index + 1}.txt", "wb") as f:
+    #     f.write(text.encode("utf-8"))
     print(f"th{index + 1} completed")
     return {index: audio_bytes}
 
@@ -78,6 +74,6 @@ def get_text():
 
 
 if __name__ == '__main__':
-    # asyncio.run(foo(20))
-    proxies = proxy_service.get_proxy_info(100)
+    asyncio.run(foo(20))
+    # proxies = proxy_service.get_proxy_info(100)
     i = 0
